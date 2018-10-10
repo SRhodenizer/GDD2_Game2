1e280e470aa40a60e1e41b5041d76a8341ade1bc
kxl3527 <kxl3527@IGM2000-12>
2018-10-10 17:57:21 -0400

    SuperBasicMovementScript

---
+++ Postmortem/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                this.transform.position += new Vector3(0.0f, 0.1f, 0.0f);
+            }
+            
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                this.transform.position += new Vector3(0.0f, -0.1f, 0.0f);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                this.transform.position += new Vector3(-0.1f, 0.0f, 0.0f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                this.transform.position += new Vector3(0.1f, 0.0f, 0.0f);
+            }
+
+
+
+
+    }
+}
