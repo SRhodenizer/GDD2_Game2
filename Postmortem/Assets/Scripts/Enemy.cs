129a6d2297e38a2b1e0cb3f598629565c827784b
acm7552 <acm7552@g.rit.edu>
2018-10-18 20:38:46 -0400

    added PowerupManager and Enemy classes

---
+++ Postmortem/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        LevelManager levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        // player = levelManager.player
+        BasicMovement movescript = player.GetComponent<BasicMovement>();
+        if (other.gameObject.tag == "player")
+        {
+            //Drop the powerup and kill the player
+        }
+
+        
+
+        
+    }
+}
---
+++ Postmortem/Assets/Scripts/PowerupManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour {
+
+    public List<GameObject> enemies;
+    public List<GameObject> powerups;
+    public GameObject enemyPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+        //enemies.Add(Instantiate(enemyPrefab));
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
